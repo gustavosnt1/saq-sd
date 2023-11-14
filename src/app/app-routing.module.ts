6a22797eba355a22239b03eb0048e9e5c032403a
Gustavo Santos <129566339+gustavosnt1@users.noreply.github.com>
2023-11-14 15:00:10 -0300

    fix: app rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FaqComponent } from './components/faq/faq.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'faq',
     component: FaqComponent
